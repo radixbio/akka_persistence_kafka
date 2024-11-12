radixbio/pekko_persistence_kafka

Migrate Akka ⟶ Pekko (#1347)
* Migrate Akka ⟶ Pekko

* format

---------

Co-authored-by: aszady <aszady@users.noreply.github.com>
GitOrigin-RevId: a27d4a98480a79b1d7da355d047a08c04d361fa6

BUILD
@@ -10,17 +10,6 @@ scala_library(
         "@maven//:com_fasterxml_jackson_core_jackson_databind",
         "@maven//:com_sksamuel_avro4s_avro4s_core_3",
         "@maven//:com_softwaremill_magnolia1_3_magnolia_3",
-        "@maven//:com_typesafe_akka_akka_actor_2_13",
-        "@maven//:com_typesafe_akka_akka_actor_typed_2_13",
-        "@maven//:com_typesafe_akka_akka_cluster_2_13",
-        "@maven//:com_typesafe_akka_akka_cluster_typed_2_13",
-        "@maven//:com_typesafe_akka_akka_coordination_2_13",
-        "@maven//:com_typesafe_akka_akka_distributed_data_2_13",
-        "@maven//:com_typesafe_akka_akka_persistence_2_13",
-        "@maven//:com_typesafe_akka_akka_protobuf_v3_2_13",
-        "@maven//:com_typesafe_akka_akka_remote_2_13",
-        "@maven//:com_typesafe_akka_akka_stream_2_13",
-        "@maven//:com_typesafe_akka_akka_stream_kafka_2_13",
         "@maven//:com_typesafe_config",
         "@maven//:io_confluent_kafka_avro_serializer",
         "@maven//:io_confluent_kafka_schema_registry_client",
@@ -29,6 +18,17 @@ scala_library(
         "@maven//:org_apache_commons_commons_compress",
         "@maven//:org_apache_kafka_kafka_clients",
         "@maven//:org_apache_kafka_kafka_server_common",
+        "@maven//:org_apache_pekko_pekko_actor_2_13",
+        "@maven//:org_apache_pekko_pekko_actor_typed_2_13",
+        "@maven//:org_apache_pekko_pekko_cluster_2_13",
+        "@maven//:org_apache_pekko_pekko_cluster_typed_2_13",
+        "@maven//:org_apache_pekko_pekko_connectors_kafka_2_13",
+        "@maven//:org_apache_pekko_pekko_coordination_2_13",
+        "@maven//:org_apache_pekko_pekko_distributed_data_2_13",
+        "@maven//:org_apache_pekko_pekko_persistence_2_13",
+        "@maven//:org_apache_pekko_pekko_protobuf_v3_2_13",
+        "@maven//:org_apache_pekko_pekko_remote_2_13",
+        "@maven//:org_apache_pekko_pekko_stream_2_13",
         "@maven//:org_lmdbjava_lmdbjava",
         "@maven//:org_reactivestreams_reactive_streams",
         "@maven//:org_slf4j_slf4j_api",
@@ -37,17 +37,6 @@ scala_library(
         ":autoserz-lib",
         "@maven//:com_sksamuel_avro4s_avro4s_core_3",
         "@maven//:com_softwaremill_magnolia1_3_magnolia_3",
-        "@maven//:com_typesafe_akka_akka_actor_2_13",
-        "@maven//:com_typesafe_akka_akka_actor_typed_2_13",
-        "@maven//:com_typesafe_akka_akka_cluster_2_13",
-        "@maven//:com_typesafe_akka_akka_cluster_typed_2_13",
-        "@maven//:com_typesafe_akka_akka_coordination_2_13",
-        "@maven//:com_typesafe_akka_akka_distributed_data_2_13",
-        "@maven//:com_typesafe_akka_akka_persistence_2_13",
-        "@maven//:com_typesafe_akka_akka_protobuf_v3_2_13",
-        "@maven//:com_typesafe_akka_akka_remote_2_13",
-        "@maven//:com_typesafe_akka_akka_stream_2_13",
-        "@maven//:com_typesafe_akka_akka_stream_kafka_2_13",
         "@maven//:com_typesafe_config",
         "@maven//:io_confluent_kafka_avro_serializer",
         "@maven//:io_confluent_kafka_schema_registry_client",
@@ -56,6 +45,17 @@ scala_library(
         "@maven//:org_apache_commons_commons_compress",
         "@maven//:org_apache_kafka_kafka_clients",
         "@maven//:org_apache_kafka_kafka_server_common",
+        "@maven//:org_apache_pekko_pekko_actor_2_13",
+        "@maven//:org_apache_pekko_pekko_actor_typed_2_13",
+        "@maven//:org_apache_pekko_pekko_cluster_2_13",
+        "@maven//:org_apache_pekko_pekko_cluster_typed_2_13",
+        "@maven//:org_apache_pekko_pekko_connectors_kafka_2_13",
+        "@maven//:org_apache_pekko_pekko_coordination_2_13",
+        "@maven//:org_apache_pekko_pekko_distributed_data_2_13",
+        "@maven//:org_apache_pekko_pekko_persistence_2_13",
+        "@maven//:org_apache_pekko_pekko_protobuf_v3_2_13",
+        "@maven//:org_apache_pekko_pekko_remote_2_13",
+        "@maven//:org_apache_pekko_pekko_stream_2_13",
         "@maven//:org_lmdbjava_lmdbjava",
         "@maven//:org_reactivestreams_reactive_streams",
         "@maven//:org_slf4j_slf4j_api",
@@ -68,22 +68,22 @@ scala_library(
     visibility = ["//shared/persistence:__subpackages__"],
     exports = [
         ":persistence-lib",
-        "@maven//:com_typesafe_akka_akka_actor_2_13",
-        "@maven//:com_typesafe_akka_akka_persistence_query_2_13",
-        "@maven//:com_typesafe_akka_akka_stream_kafka_2_13",
         "@maven//:io_confluent_kafka_avro_serializer",
+        "@maven//:org_apache_pekko_pekko_actor_2_13",
+        "@maven//:org_apache_pekko_pekko_connectors_kafka_2_13",
+        "@maven//:org_apache_pekko_pekko_persistence_query_2_13",
     ],
     deps = [
         ":persistence-lib",
-        "@maven//:com_typesafe_akka_akka_actor_2_13",
-        "@maven//:com_typesafe_akka_akka_persistence_query_2_13",
-        "@maven//:com_typesafe_akka_akka_stream_kafka_2_13",
+        "@maven//:org_apache_pekko_pekko_actor_2_13",
+        #        "@maven//:org_apache_pekko_pekko_connectors_kafka_2_13",
+        "@maven//:org_apache_pekko_pekko_persistence_query_2_13",
         "@maven//:io_confluent_kafka_avro_serializer",
     ],
 )
 
 scala_test(
-    name = "akka-persistence-tck",
+    name = "pekko-persistence-tck",
     srcs = [
         "src/test/scala/KafkaJournalTest.scala",
         "src/test/scala/KafkaSnapshotStoreTest.scala",
@@ -92,9 +92,6 @@ scala_test(
     resources = ["src/test/resources/application.conf"],
     deps = [
         ":persistence-lib",
-        "@maven//:com_typesafe_akka_akka_cluster_typed_2_13",
-        "@maven//:com_typesafe_akka_akka_persistence_tck_2_13",
-        "@maven//:com_typesafe_akka_akka_testkit_2_13",
         "@maven//:com_typesafe_scala_logging_scala_logging_2_13",
         "@maven//:com_yammer_metrics_metrics_core",
         "@maven//:io_confluent_kafka_schema_registry",
@@ -103,6 +100,9 @@ scala_test(
         "@maven//:io_github_embeddedkafka_embedded_kafka_schema_registry_2_13",
         "@maven//:org_apache_kafka_kafka_2_13",
         "@maven//:org_apache_kafka_kafka_metadata",
+        "@maven//:org_apache_pekko_pekko_cluster_typed_2_13",
+        "@maven//:org_apache_pekko_pekko_persistence_tck_2_13",
+        "@maven//:org_apache_pekko_pekko_testkit_2_13",
         "@maven//:org_apache_zookeeper_zookeeper",
         "@maven//:org_scalatest_scalatest_3",
         "@maven//:org_scalatest_scalatest_compatible",
@@ -126,9 +126,6 @@ scala_test(
     deps = [
         ":persistence-lib",
         "@maven//:com_sksamuel_avro4s_avro4s_core_3",
-        "@maven//:com_typesafe_akka_akka_actor_testkit_typed_3",
-        "@maven//:com_typesafe_akka_akka_cluster_typed_2_13",
-        "@maven//:com_typesafe_akka_akka_testkit_2_13",
         "@maven//:com_typesafe_config",
         "@maven//:com_yammer_metrics_metrics_core",
         "@maven//:org_apache_kafka_kafka_group_coordinator",
@@ -136,6 +133,9 @@ scala_test(
         "@maven//:org_apache_kafka_kafka_raft",
         "@maven//:org_apache_kafka_kafka_server_common",
         "@maven//:org_apache_kafka_kafka_storage",
+        "@maven//:org_apache_pekko_pekko_actor_testkit_typed_3",
+        "@maven//:org_apache_pekko_pekko_cluster_typed_2_13",
+        "@maven//:org_apache_pekko_pekko_testkit_2_13",
         "@maven//:org_apache_zookeeper_zookeeper",
         "@maven//:org_apache_zookeeper_zookeeper_jute",
         "@maven//:org_scalatest_scalatest_3",
@@ -163,7 +163,6 @@ scala_test(
     deps = [
         ":persistence",
         "//test:test-lib",
-        "@maven//:com_typesafe_akka_akka_actor_typed_2_13",
         "@maven//:com_yammer_metrics_metrics_core",
         "@maven//:io_github_embeddedkafka_embedded_kafka_schema_registry_2_13",
         "@maven//:org_apache_kafka_kafka_group_coordinator",
@@ -171,6 +170,7 @@ scala_test(
         "@maven//:org_apache_kafka_kafka_raft",
         "@maven//:org_apache_kafka_kafka_server_common",
         "@maven//:org_apache_kafka_kafka_storage",
+        "@maven//:org_apache_pekko_pekko_actor_typed_2_13",
         "@maven//:org_apache_zookeeper_zookeeper",
         "@maven//:org_apache_zookeeper_zookeeper_jute",
         "@maven//:org_scalatest_scalatest_3",
@@ -194,14 +194,14 @@ scala_library(
     visibility = ["//shared/persistence:__subpackages__"],
     exports = [
         "//test:test-lib",
-        "@maven//:com_typesafe_akka_akka_actor_testkit_typed_3",
-        "@maven//:com_typesafe_akka_akka_testkit_2_13",
         "@maven//:com_yammer_metrics_metrics_core",
         "@maven//:org_apache_kafka_kafka_group_coordinator",
         "@maven//:org_apache_kafka_kafka_metadata",
         "@maven//:org_apache_kafka_kafka_raft",
         "@maven//:org_apache_kafka_kafka_server_common",
         "@maven//:org_apache_kafka_kafka_storage",
+        "@maven//:org_apache_pekko_pekko_actor_testkit_typed_3",
+        "@maven//:org_apache_pekko_pekko_testkit_2_13",
         "@maven//:org_apache_zookeeper_zookeeper",
         "@maven//:org_apache_zookeeper_zookeeper_jute",
         "@maven//:org_scalactic_scalactic_3",
@@ -220,14 +220,14 @@ scala_library(
     ],
     deps = [
         "//test:test-lib",
-        "@maven//:com_typesafe_akka_akka_actor_testkit_typed_3",
-        "@maven//:com_typesafe_akka_akka_testkit_2_13",
         "@maven//:com_yammer_metrics_metrics_core",
         "@maven//:org_apache_kafka_kafka_group_coordinator",
         "@maven//:org_apache_kafka_kafka_metadata",
         "@maven//:org_apache_kafka_kafka_raft",
         "@maven//:org_apache_kafka_kafka_server_common",
         "@maven//:org_apache_kafka_kafka_storage",
+        "@maven//:org_apache_pekko_pekko_actor_testkit_typed_3",
+        "@maven//:org_apache_pekko_pekko_testkit_2_13",
         "@maven//:org_apache_zookeeper_zookeeper",
         "@maven//:org_apache_zookeeper_zookeeper_jute",
         "@maven//:org_scalactic_scalactic_3",

radixbio/akka_persistence_kafka

BUILD
@@ -10,17 +10,6 @@ scala_library(
         "@maven//:com_fasterxml_jackson_core_jackson_databind",
         "@maven//:com_sksamuel_avro4s_avro4s_core_3",
         "@maven//:com_softwaremill_magnolia1_3_magnolia_3",
-        "@maven//:com_typesafe_akka_akka_actor_2_13",
-        "@maven//:com_typesafe_akka_akka_actor_typed_2_13",
-        "@maven//:com_typesafe_akka_akka_cluster_2_13",
-        "@maven//:com_typesafe_akka_akka_cluster_typed_2_13",
-        "@maven//:com_typesafe_akka_akka_coordination_2_13",
-        "@maven//:com_typesafe_akka_akka_distributed_data_2_13",
-        "@maven//:com_typesafe_akka_akka_persistence_2_13",
-        "@maven//:com_typesafe_akka_akka_protobuf_v3_2_13",
-        "@maven//:com_typesafe_akka_akka_remote_2_13",
-        "@maven//:com_typesafe_akka_akka_stream_2_13",
-        "@maven//:com_typesafe_akka_akka_stream_kafka_2_13",
         "@maven//:com_typesafe_config",
         "@maven//:io_confluent_kafka_avro_serializer",
         "@maven//:io_confluent_kafka_schema_registry_client",
@@ -29,6 +18,17 @@ scala_library(
         "@maven//:org_apache_commons_commons_compress",
         "@maven//:org_apache_kafka_kafka_clients",
         "@maven//:org_apache_kafka_kafka_server_common",
+        "@maven//:com_typesafe_akka_akka_actor_2_13",
+        "@maven//:com_typesafe_akka_akka_actor_typed_2_13",
+        "@maven//:com_typesafe_akka_akka_cluster_2_13",
+        "@maven//:com_typesafe_akka_akka_cluster_typed_2_13",
+        "@maven//:com_typesafe_akka_akka_connectors_kafka_2_13",
+        "@maven//:com_typesafe_akka_akka_coordination_2_13",
+        "@maven//:com_typesafe_akka_akka_distributed_data_2_13",
+        "@maven//:com_typesafe_akka_akka_persistence_2_13",
+        "@maven//:com_typesafe_akka_akka_protobuf_v3_2_13",
+        "@maven//:com_typesafe_akka_akka_remote_2_13",
+        "@maven//:com_typesafe_akka_akka_stream_2_13",
         "@maven//:org_lmdbjava_lmdbjava",
         "@maven//:org_reactivestreams_reactive_streams",
         "@maven//:org_slf4j_slf4j_api",
@@ -37,17 +37,6 @@ scala_library(
         ":autoserz-lib",
         "@maven//:com_sksamuel_avro4s_avro4s_core_3",
         "@maven//:com_softwaremill_magnolia1_3_magnolia_3",
-        "@maven//:com_typesafe_akka_akka_actor_2_13",
-        "@maven//:com_typesafe_akka_akka_actor_typed_2_13",
-        "@maven//:com_typesafe_akka_akka_cluster_2_13",
-        "@maven//:com_typesafe_akka_akka_cluster_typed_2_13",
-        "@maven//:com_typesafe_akka_akka_coordination_2_13",
-        "@maven//:com_typesafe_akka_akka_distributed_data_2_13",
-        "@maven//:com_typesafe_akka_akka_persistence_2_13",
-        "@maven//:com_typesafe_akka_akka_protobuf_v3_2_13",
-        "@maven//:com_typesafe_akka_akka_remote_2_13",
-        "@maven//:com_typesafe_akka_akka_stream_2_13",
-        "@maven//:com_typesafe_akka_akka_stream_kafka_2_13",
         "@maven//:com_typesafe_config",
         "@maven//:io_confluent_kafka_avro_serializer",
         "@maven//:io_confluent_kafka_schema_registry_client",
@@ -56,6 +45,17 @@ scala_library(
         "@maven//:org_apache_commons_commons_compress",
         "@maven//:org_apache_kafka_kafka_clients",
         "@maven//:org_apache_kafka_kafka_server_common",
+        "@maven//:com_typesafe_akka_akka_actor_2_13",
+        "@maven//:com_typesafe_akka_akka_actor_typed_2_13",
+        "@maven//:com_typesafe_akka_akka_cluster_2_13",
+        "@maven//:com_typesafe_akka_akka_cluster_typed_2_13",
+        "@maven//:com_typesafe_akka_akka_connectors_kafka_2_13",
+        "@maven//:com_typesafe_akka_akka_coordination_2_13",
+        "@maven//:com_typesafe_akka_akka_distributed_data_2_13",
+        "@maven//:com_typesafe_akka_akka_persistence_2_13",
+        "@maven//:com_typesafe_akka_akka_protobuf_v3_2_13",
+        "@maven//:com_typesafe_akka_akka_remote_2_13",
+        "@maven//:com_typesafe_akka_akka_stream_2_13",
         "@maven//:org_lmdbjava_lmdbjava",
         "@maven//:org_reactivestreams_reactive_streams",
         "@maven//:org_slf4j_slf4j_api",
@@ -68,22 +68,22 @@ scala_library(
     visibility = ["//shared/persistence:__subpackages__"],
     exports = [
         ":persistence-lib",
-        "@maven//:com_typesafe_akka_akka_actor_2_13",
-        "@maven//:com_typesafe_akka_akka_persistence_query_2_13",
-        "@maven//:com_typesafe_akka_akka_stream_kafka_2_13",
         "@maven//:io_confluent_kafka_avro_serializer",
+        "@maven//:com_typesafe_akka_akka_actor_2_13",
+        "@maven//:com_typesafe_akka_akka_connectors_kafka_2_13",
+        "@maven//:com_typesafe_akka_akka_persistence_query_2_13",
     ],
     deps = [
         ":persistence-lib",
         "@maven//:com_typesafe_akka_akka_actor_2_13",
+        #        "@maven//:com_typesafe_akka_akka_connectors_kafka_2_13",
         "@maven//:com_typesafe_akka_akka_persistence_query_2_13",
-        "@maven//:com_typesafe_akka_akka_stream_kafka_2_13",
         "@maven//:io_confluent_kafka_avro_serializer",
     ],
 )
 
 scala_test(
-    name = "akka-persistence-tck",
+    name = "pekko-persistence-tck",
     srcs = [
         "src/test/scala/KafkaJournalTest.scala",
         "src/test/scala/KafkaSnapshotStoreTest.scala",
@@ -92,9 +92,6 @@ scala_test(
     resources = ["src/test/resources/application.conf"],
     deps = [
         ":persistence-lib",
-        "@maven//:com_typesafe_akka_akka_cluster_typed_2_13",
-        "@maven//:com_typesafe_akka_akka_persistence_tck_2_13",
-        "@maven//:com_typesafe_akka_akka_testkit_2_13",
         "@maven//:com_typesafe_scala_logging_scala_logging_2_13",
         "@maven//:com_yammer_metrics_metrics_core",
         "@maven//:io_confluent_kafka_schema_registry",
@@ -103,6 +100,9 @@ scala_test(
         "@maven//:io_github_embeddedkafka_embedded_kafka_schema_registry_2_13",
         "@maven//:org_apache_kafka_kafka_2_13",
         "@maven//:org_apache_kafka_kafka_metadata",
+        "@maven//:com_typesafe_akka_akka_cluster_typed_2_13",
+        "@maven//:com_typesafe_akka_akka_persistence_tck_2_13",
+        "@maven//:com_typesafe_akka_akka_testkit_2_13",
         "@maven//:org_apache_zookeeper_zookeeper",
         "@maven//:org_scalatest_scalatest_3",
         "@maven//:org_scalatest_scalatest_compatible",
@@ -126,9 +126,6 @@ scala_test(
     deps = [
         ":persistence-lib",
         "@maven//:com_sksamuel_avro4s_avro4s_core_3",
-        "@maven//:com_typesafe_akka_akka_actor_testkit_typed_3",
-        "@maven//:com_typesafe_akka_akka_cluster_typed_2_13",
-        "@maven//:com_typesafe_akka_akka_testkit_2_13",
         "@maven//:com_typesafe_config",
         "@maven//:com_yammer_metrics_metrics_core",
         "@maven//:org_apache_kafka_kafka_group_coordinator",
@@ -136,6 +133,9 @@ scala_test(
         "@maven//:org_apache_kafka_kafka_raft",
         "@maven//:org_apache_kafka_kafka_server_common",
         "@maven//:org_apache_kafka_kafka_storage",
+        "@maven//:com_typesafe_akka_akka_actor_testkit_typed_3",
+        "@maven//:com_typesafe_akka_akka_cluster_typed_2_13",
+        "@maven//:com_typesafe_akka_akka_testkit_2_13",
         "@maven//:org_apache_zookeeper_zookeeper",
         "@maven//:org_apache_zookeeper_zookeeper_jute",
         "@maven//:org_scalatest_scalatest_3",
@@ -163,7 +163,6 @@ scala_test(
     deps = [
         ":persistence",
         "//test:test-lib",
-        "@maven//:com_typesafe_akka_akka_actor_typed_2_13",
         "@maven//:com_yammer_metrics_metrics_core",
         "@maven//:io_github_embeddedkafka_embedded_kafka_schema_registry_2_13",
         "@maven//:org_apache_kafka_kafka_group_coordinator",
@@ -171,6 +170,7 @@ scala_test(
         "@maven//:org_apache_kafka_kafka_raft",
         "@maven//:org_apache_kafka_kafka_server_common",
         "@maven//:org_apache_kafka_kafka_storage",
+        "@maven//:com_typesafe_akka_akka_actor_typed_2_13",
         "@maven//:org_apache_zookeeper_zookeeper",
         "@maven//:org_apache_zookeeper_zookeeper_jute",
         "@maven//:org_scalatest_scalatest_3",
@@ -194,14 +194,14 @@ scala_library(
     visibility = ["//shared/persistence:__subpackages__"],
     exports = [
         "//test:test-lib",
-        "@maven//:com_typesafe_akka_akka_actor_testkit_typed_3",
-        "@maven//:com_typesafe_akka_akka_testkit_2_13",
         "@maven//:com_yammer_metrics_metrics_core",
         "@maven//:org_apache_kafka_kafka_group_coordinator",
         "@maven//:org_apache_kafka_kafka_metadata",
         "@maven//:org_apache_kafka_kafka_raft",
         "@maven//:org_apache_kafka_kafka_server_common",
         "@maven//:org_apache_kafka_kafka_storage",
+        "@maven//:com_typesafe_akka_akka_actor_testkit_typed_3",
+        "@maven//:com_typesafe_akka_akka_testkit_2_13",
         "@maven//:org_apache_zookeeper_zookeeper",
         "@maven//:org_apache_zookeeper_zookeeper_jute",
         "@maven//:org_scalactic_scalactic_3",
@@ -220,14 +220,14 @@ scala_library(
     ],
     deps = [
         "//test:test-lib",
-        "@maven//:com_typesafe_akka_akka_actor_testkit_typed_3",
-        "@maven//:com_typesafe_akka_akka_testkit_2_13",
         "@maven//:com_yammer_metrics_metrics_core",
         "@maven//:org_apache_kafka_kafka_group_coordinator",
         "@maven//:org_apache_kafka_kafka_metadata",
         "@maven//:org_apache_kafka_kafka_raft",
         "@maven//:org_apache_kafka_kafka_server_common",
         "@maven//:org_apache_kafka_kafka_storage",
+        "@maven//:com_typesafe_akka_akka_actor_testkit_typed_3",
+        "@maven//:com_typesafe_akka_akka_testkit_2_13",
         "@maven//:org_apache_zookeeper_zookeeper",
         "@maven//:org_apache_zookeeper_zookeeper_jute",
         "@maven//:org_scalactic_scalactic_3",